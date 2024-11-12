f0c322bc8e41eb752e74447a4842dd0c8da846b9
Darien Esteban Vesga Carvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-12 04:28:09 +0000

    temperatureConversion

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-#Escribe un programa que asigne una calificación basada en una nota numérica.
+#Escribe un programa que convierta grados Celsius a Fahrenheit o Fahrenheit a Celsius usando match.
 
-grade = float(input("What's the grade in number?: "))
+temperature = float(input("What is the temperature?: "))
+scale = input("What is the temperature scale? (C or F): ")
 
-if grade >= 90 and grade <= 100:
-    print("The grade is A.")
-elif grade >= 80 and grade < 90:
-    print("The grade is B.")
-elif grade >= 70 and grade < 80:
-    print("The grade is C.")
-elif grade >= 60 and grade < 70:
-    print("The grade is D.")
-elif grade >= 0 and grade < 60:
-    print("The grade is F.")
-else:
-    print("Error: Invalid grade.")
+match scale:
+    case "C":
+        convertedTemperature = (((9 / 5) * temperature) + 32)
+        convertedScale = "F"
+        print(f"The temperature is {convertedTemperature:.2f}{convertedScale}.")
+    case "F":
+        convertedTemperature = ((5 / 9) * (temperature - 32))
+        convertedScale = "C"
+        print(f"The temperature is {convertedTemperature:.2f}{convertedScale}.")
+    case _:
+        print("Error: Invalid scale.")
