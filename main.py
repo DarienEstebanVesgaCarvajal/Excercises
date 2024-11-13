6a53bec5e838569e42493db521ff3b40b0050289
Darien Esteban Vesga Carvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-13 03:05:00 +0000

    numCredits

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-#Escribe un programa que calcule la calificación final de un estudiante basándose en su calificación y si ha hecho tareas adicionales. Las tareas adicionales pueden darle un extra de puntos, pero el máximo de puntos no puede exceder 100.
+#Escribe un programa que calcule el número de créditos totales de un estudiante en base a las materias cursadas y el puntaje obtenido en cada una. El puntaje debe ser evaluado como aprobado o no aprobado.
 
-grade = float(input("What's your grade?: "))
-additionalTasks = input("""Have you done additional tasks?
-    Y (Yes).
-    N (Not).
-( Y | N ): """)
+numSubjects = int(input("How many school subjects have you completed?: "))
+credits = 0
 
-additionalTasks = additionalTasks.upper()
+for x in range(numSubjects):
+    score = float(input(f"""What's your score?: """))
 
-while True:
+    if score >= 60:
+        credits += 3
 
-    match additionalTasks:
-        case "Y":
-            finalGrade = grade + (0.05 * grade)
-            if finalGrade > 100:
-                finalGrade = 100
-                print(f"""You have a grade of: {finalGrade:.2f}.""")
-            else:
-                print(f"""You have a grade of: {finalGrade:.2f}.""")
-            break
-        case "N":
-            print(f"""You have a grade of: {grade:.2f}. """)
-            break
-        case _:
-            print("Error: Wrong character.")
-            additionalTasks = input("""Have you done additional tasks?
-    Y (Yes).
-    N (Not).
-( Y | N ): """)
-            additionalTasks = additionalTasks.upper()
+print(f"""Total number of credits: {credits}.""")
